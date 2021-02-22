c8be7ff586073d60df730b653693f36553ce56b9
kkkooosss <kkacar@mail.ru>
2021-02-22 17:48:42 +0200

    first commit

--- minikube_install_yum.sh
+++ minikube_install_yum.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-yum update -y
+sudo yum update -y
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-install minikube-linux-amd64 /usr/local/bin/minikube
-yum install conntrack -y
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo yum install conntrack -y
 minikube start --vm-driver=none
 
 
