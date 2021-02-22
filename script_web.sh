5b6032a0a05222585fae69851cbf55947d375c7f
kkkooosss <kkacar@mail.ru>
2021-02-22 10:26:42 +0200

    init commit

--- script_web.sh
+++ script_web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PublicIP=`ifconfig | grep  "netmask 255.255.255.0" | grep inet | awk -F'[: ]+' '{ print $3 }'`
-Hostname=`hostname`
+PublicIP=`curl http://169.254.169.254/latest/meta-data/public-ipv4/'`
+Hostname=`curl http://169.254.169.254/latest/meta-data/hostname`
 echo "<html><body bgcolor=black><center><h1><p><font color=yellow>Web Server: $Hostname </h1></center></body><html>   <html><body bgcolor=black><center><h1><p><font color=red>Web Server IP: $PublicIP </h1></center></body><html>" > index.html
