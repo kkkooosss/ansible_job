9349395cab28cba7b41d5314ea761c1e92088a3a
kkkooosss <kkacar@mail.ru>
2021-02-22 15:03:03 +0200

    first commit

---
+++ kubectl_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+openssl sha1 -sha256 kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
