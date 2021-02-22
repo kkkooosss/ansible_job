2ad74dd9f9fe112e37563c0e269d4aaef061d65e
kkkooosss <kkacar@mail.ru>
2021-02-22 10:08:20 +0200

    init commit

--- app_deployment.sh
+++ app_deployment.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/kkkooosss/ansible_job.git
 cd ansible_job/
 ./script_web.sh
-kubectl apply -f app-deployment.yaml
+kubectl apply -f app_deployment.yaml
 PodName=`kubectl get pod | grep nginx-deploy* |awk -F'[: ]+' '{ print $1 }'`
 kubectl cp index.html $PodName:/usr/share/nginx/html 
 kubectl expose deployment nginx-deploy --type=NodePort --port=80
