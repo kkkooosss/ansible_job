df213e8b320ccaad16bd21e1f921db1bd6350041
kkkooosss <kkacar@mail.ru>
2021-02-22 17:18:44 +0200

    first commit

--- minikube_install_yum.sh
+++ minikube_install_yum.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo yum install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
-openssl sha1 -sha256 kubectl
-chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
-echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
-kubectl version --short --client
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
-sudo rpm -ivh minikube-latest.x86_64.rpm
-sudo yum install conntrack -y
-sudo minikube start --vm-driver=none
+yum update -y
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
+yum install conntrack -y
+minikube start --vm-driver=none
+
+
 
