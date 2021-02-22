4adc4bca6579c77d267ffff6e44036e7246b11ae
kkkooosss <kkacar@mail.ru>
2021-02-22 15:10:30 +0200

    first commit

--- script_web.sh
+++ script_web.sh
@@ -2,4 +2,5 @@
 PublicIP=`curl http://169.254.169.254/latest/meta-data/public-ipv4/`
 Hostname=`curl http://169.254.169.254/latest/meta-data/hostname`
 echo "<html><body bgcolor=black><center><h1><p><font color=yellow>Web Server: $Hostname </h1></center></body><html>   <html><body bgcolor=black><center><h1><p><font color=red>Web Server IP: $PublicIP </h1></center></body><html>" > index.html
+git clone https://github.com/kkkooosss/ansible_job.git
 cd ans_job_git/
